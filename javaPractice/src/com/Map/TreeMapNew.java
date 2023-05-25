f38256862b81e0a06278f0eb22686a93722bf0f5
tarun_kumar <tarun.kumar@bonamisoftware.com>
2023-05-25 17:05:07 +0530

    pushing from terminal

---
+++ javaPractice/src/com/Map/TreeMapNew.java
@@ -0,0 +1,11 @@
+package com.Map;
+
+import java.util.TreeMap;
+
+public class TreeMapNew {
+	public static void main(String args[]) {
+		TreeMap map = new TreeMap();
+		map.put(2,null);
+		System.out.println(map);
+	}
+}
